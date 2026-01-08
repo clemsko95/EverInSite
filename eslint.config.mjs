00a633a9b4a6deae308e443bb0d05752e5ac5980
clemsko95 <clem952@outlook.fr>
2026-01-08 18:00:08 +0100

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,10 @@
 import { FlatCompat } from '@eslint/eslintrc'
- 
+
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
+  // import.meta.dirname is disponible après Node.js v20.11.0
   baseDirectory: import.meta.dirname,
 })
- 
+
 const eslintConfig = [
   ...compat.config({
     extends: ['next'],
@@ -14,9 +14,9 @@ const eslintConfig = [
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-img-element': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
       'react-hooks/exhaustive-deps': 'off',
+
+      // règles import
       'import/no-unresolved': 'error',
       'import/named': 'error',
       'import/default': 'error',
@@ -29,5 +29,5 @@ const eslintConfig = [
     },
   },
 ]
- 
+
 export default eslintConfig
